1f7e257bcec040eb8a49245d8e412033218e9f61
arunankathina <arunank@athina.ai>
2025-01-21 15:53:40 +0530

    add patch for pwdmodule.c

--- Modules/pwdmodule.c
+++ Modules/pwdmodule.c
@@ -290,7 +290,15 @@ Return a list of all available password database entries, in arbitrary order.
 See help(pwd) for more on password database entries.
 [clinic start generated code]*/
 
-
+#ifdef __ANDROID__
+static PyObject *
+pwd_getpwall_impl(PyObject *module)
+{
+    PyErr_SetString(PyExc_NotImplementedError,
+                    "passwd database functions are not available on Android.");
+    return NULL;
+}
+#else
 static PyObject *
 pwd_getpwall_impl(PyObject *module)
 /*[clinic end generated code: output=4853d2f5a0afac8a input=d7ecebfd90219b85]*/
@@ -314,6 +322,8 @@ pwd_getpwall_impl(PyObject *module)
     return d;
 }
 #endif
+#endif
+
 
 static PyMethodDef pwd_methods[] = {
     PWD_GETPWUID_METHODDEF
